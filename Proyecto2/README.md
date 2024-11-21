7858e5d66f6e47b4407d0ee6520d56a949d99d2e
kellylopez1 <167722462+kellylopez1@users.noreply.github.com>
2024-11-20 23:19:16 -0400

    actualizacion

---
+++ Proyecto2/README.md
@@ -0,0 +1,2 @@
+# Proyecto2_Arbol_Genealogico_UNIMET
+Proyecto 2 - Nicole Tolve y Kelly Lopez
---
+++ Proyecto2/src/main/java/Proyecto2/Proyecto2_Kelly.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Proyecto2;
+
+/**
+ *
+ * @author kelly
+ */
+public class Proyecto2_Kelly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
